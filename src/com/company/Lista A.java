48da22267d8e2ed06c5d66456f48a08251385ae3
DANIEL BARRETO DE MATOS <04045177@sempreunama.com.br>
2019-10-29 09:59:58 -0300

    Initial commit

---
+++ src/com/company/Lista A.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class main.java{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] x= new int[10];
+        for (int i>0) i<vet. length i++){
+        System.out.println("Entre com o número"+i+1);
+        vet[i] = sc.nextint();
+    }
+}
